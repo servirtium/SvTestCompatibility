1bb5a49749c0a9fd456f9a0b675cf1667988c1c7
Paul Hammant <paul@hammant.org>
2024-06-30 20:59:07 +0100

    work

--- java/README.md
+++ java/README.md
@@ -1,6 +1,7 @@
 
 
 ```
+apt install subversion maven
 mvn install
-java -cp target/servirtium-example-1.0-SNAPSHOT.jar Recorder
+java -cp target/servirtium-example-1.0-SNAPSHOT.jar com.example.Recorder
 ```
--- java/src/main/java/com/example/Recorder.java
+++ java/src/main/java/com/example/Recorder.java
@@ -1,30 +1,49 @@
 package com.example;
 
 import org.http4k.client.ApacheClient;
-import org.http4k.core.Method;
-import org.http4k.core.Request;
-import org.http4k.core.Response;
-import org.http4k.core.Status;
-import org.http4k.core.Uri;
+import org.http4k.core.*;
 import org.http4k.server.SunHttp;
 import org.http4k.servirtium.InteractionStorage;
 import org.http4k.servirtium.InteractionOptions;
 import org.http4k.servirtium.ServirtiumServer;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class Recorder {
-    public static void main(String[] args) throws IOException {
-        String baseUrl = "https://svn.apache.org";
-        String interactionFile = "ExampleSubversionCheckoutRecording.md";
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        InteractionOptions io = new  InteractionOptions() {
+
+            public Request modify(Request request) {
+                return request
+                        .replaceHeader("Host", "localhost")
+                        .replaceHeader("User-agent", "Svn CLI")
+                        .replaceHeader("Date", "Tue, 28 Jan 2020 14:15:55 GMT");
+            }
+
+            public Response modify(Response response) {
+                return response
+                        //.removeHeaders("X-")
+                        //.removeHeader("Set-Cookie")
+                        .replaceHeader("Date", "Tue, 28 Jan 2020 14:15:55 GMT");
+            }
+
+            public boolean isBinary(ContentType contentType) {
+                return false;
+            }
+
+            public boolean debugTraffic() {
+                return false;
+            }
+        };
 
         ServirtiumServer server = ServirtiumServer.Recording(
-            "test",
-            Uri.of(baseUrl),
-            InteractionStorage.Disk(new File(interactionFile)),
-            InteractionOptions.Defaults,
-            61417,
+            "ExampleSubversionCheckoutRecording", Uri.of("https://svn.apache.org"),
+            InteractionStorage.Disk(new File("..")),
+            io, 61417,
             SunHttp::new,
             ApacheClient.create()
         );
@@ -32,22 +51,14 @@ public class Recorder {
         server.start();
         System.out.println("Recording started");
 
-        try {
-            TimeUnit.SECONDS.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        TimeUnit.SECONDS.sleep(5);
 
         ProcessBuilder pb = new ProcessBuilder("svn", "co", "http://localhost:61417/repos/asf/synapse/tags/3.0.0/modules/distribution/src/main/conf");
         pb.inheritIO();
-        try {
-            Process process = pb.start();
-            int exitCode = process.waitFor();
-            if (exitCode != 0) {
-                System.err.println("SVN command failed with exit code " + exitCode);
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
+        Process process = pb.start();
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            System.err.println("SVN command failed with exit code " + exitCode);
         }
 
         server.stop();
